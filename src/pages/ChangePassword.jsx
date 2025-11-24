e673eb85caa42cf1faf8ed5234342afb89a3bf22
mukesh-silwal <mukesh.silwal@f1soft.com>
2025-11-24 13:29:11 +0545

    cc

--- src/pages/ChangePassword.jsx
+++ src/pages/ChangePassword.jsx
@@ -33,7 +33,7 @@ export default function ChangePassword() {
         username: email,
       });
 
-      if (response) {
+      if (response.ok) {
         toast.success("OTP sent");
         setOtpModalVisible(true);
       } else {
